f7c77e4dd3341b8ea9268a6a5ea2606a56fe81e4
ANDREA VONA <andrea.vona23@gmail.com>
2022-09-18 15:06:41 +0200

    implemented folders' synchronization part

--- synchronizationScript.py
+++ synchronizationScript.py
@@ -1,8 +1,7 @@
 from genericpath import isfile
 import sys
 import os
-import hashlib
-from turtle import title
+from shutil import copytree, rmtree
 
 # parameters:
 # folder source [path]
@@ -22,28 +21,33 @@ if (replica[-1]!='/'):
     replica = replica + '/'
 
 
+# hash of name and content of a file
+def getFileHash(folder, fileName):
+    fname = folder + fileName
+    f = open(fname)
+    content =  f.read()
+    f.close()
+    h = hash(content) + hash(fileName)
+    return h
+
+
 # we split the files and the directories of the two folders
 # because we will need the hash of the files later, the first return
-# will be a dict {file1-title: hash1, ..., fileN-title:hashN}
+# will be a dict {hash1: file1, ..., hashN:fileN}, the second a list
+# directories' names
+# @param folder is the full path of the folder [String]
 def getFilesAndFolders(folder):
     paths = os.listdir(folder)
     files = {}
-    dir = []
+    dirs = []
     for p in paths:
         if os.path.isfile(os.path.join(folder, p)):
-            fname = folder + p
-            f = open(fname)
-            content =  f.read()
-            f.close()
-            h = hash(content) + hash(p)
+            h = getFileHash(folder, p)
             files[h] = p
         else:
-            dir.append(p)
-    return files, dir
+            dirs.append(p)
+    return files, dirs
 
-
-srcFiles, srcDirs = getFilesAndFolders(src)
-replicaFiles, replicaDirs = getFilesAndFolders(replica)
 
 
 # auxiliary function to copy a file
@@ -58,7 +62,12 @@ def copyFile(nameReplicaFile, nameSrcFile):
 # For the files creation, we have two subcases:
 # 1. the hashes match meaning that the files are already present
 # 2. the hash don't match meaning we have to create a new file
-def syncFiles(srcFiles, replicaFiles):
+# params:
+# srcFiles [list of names]
+# replicaFiles [list of names]
+# src [string] folder path
+# replica [string] folder path
+def syncFiles(srcFiles, replicaFiles, src, replica):
     for srcFileHash in srcFiles:
         replicaFilePath = replica + srcFiles[srcFileHash]
         srcFilePath = src + srcFiles[srcFileHash]
@@ -78,6 +87,70 @@ def syncFiles(srcFiles, replicaFiles):
         if (replicaFileHash not in srcFiles):
             os.remove(replicaFilePath)
 
-syncFiles(srcFiles, replicaFiles)
+# computes a folder hash
+# takes as parameter the full folder path
+def getFolderHash(folder):
+    if (folder[-1]!='/'):
+        folder = folder + '/'
+    paths = os.listdir(folder)
+    #print(paths)
+    if (len(paths)==0):
+        nameDir = folder.split('/')[-2]
+        return hash(nameDir)
+    else:
+        folderFiles, folderDirs = getFilesAndFolders(folder)
+        hashesFilesSum = 0
+        hashesDirsSum = 0
+        for h in folderFiles:
+            hashesFilesSum = hashesFilesSum + h
+        for d in folderDirs:
+            folderPath = folder + d
+            hashesDirsSum = hashesDirsSum + getFolderHash(folderPath)
+        return hashesFilesSum + hashesDirsSum
 
-# to implement folders' synchronization
+# first part, folders' creation:
+# if the src folder directory name is present between the replicas
+# synchronizes the files and keeps digging into the src folder directory
+# otherwise it creates the directory
+# second part, folders' deletion:
+# having already created all the folders in the replica, we proceed by eliminating
+# all the folders not present in the src
+# parameters:
+# list of source directories
+# list of replica directories
+# string, full src path
+# string, full replica path
+def syncFolders(srcDirs, replicaDirs, src, replica):
+    for srcDir in srcDirs:
+        srcDirPath = src + srcDir + '/'
+        
+        # srcDir is in the replicaDirs
+        try:
+            dirIndex = replicaDirs.index(srcDir)
+            replicaDir = replicaDirs[dirIndex]
+            replicaDirPath = replica + replicaDir + '/'
+            srcDirFiles, srcDirDirs = getFilesAndFolders(srcDirPath)
+            replicaDirFiles, replicaDirDirs = getFilesAndFolders(replicaDirPath)
+            syncFiles(srcDirFiles, replicaDirFiles, srcDirPath, replicaDirPath)
+            syncFolders(srcDirDirs, replicaDirDirs, srcDirPath, replicaDirPath)
+
+        # srcDir is not in the replicaDirs -> create the dir inside of it
+        except:
+            print(srcDirPath)
+            print(replica)
+            replicaDirPath = replica + srcDir + '/'
+            copytree(srcDirPath, replicaDirPath)
+
+    # remvose all the directories not present in the src folder but 
+    # in the replica folders
+    for replicaDir in replicaDirs:
+        if replicaDir not in srcDirs:
+            replicaDirPath = replica + replicaDir + '/'
+            rmtree(replicaDirPath)
+
+
+
+srcFiles, srcDirs = getFilesAndFolders(src)
+replicaFiles, replicaDirs = getFilesAndFolders(replica)
+syncFiles(srcFiles, replicaFiles, src, replica)
+syncFolders(srcDirs, replicaDirs, src, replica)
